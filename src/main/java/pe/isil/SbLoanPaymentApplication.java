c922f383b13f05b435226a2dd1f46cdc51e35322
Jose Ventura <ventura.jos3@gmail.com>
2020-05-21 18:55:54 -0500

    first commit

---
+++ src/main/java/pe/isil/SbLoanPaymentApplication.java
@@ -0,0 +1,13 @@
+package pe.isil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbLoanPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbLoanPaymentApplication.class, args);
+    }
+
+}
